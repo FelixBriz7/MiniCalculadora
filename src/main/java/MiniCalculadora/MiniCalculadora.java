aa44539d0aa6793c9cde9793cc36e77dd149959d
FelixBriz7 <felixbriz077@gmail.com>
2026-04-06 12:29:22 +0200

    Metodo de raiz cuadrada en la clase MiniCalculadora

---
+++ src/main/java/MiniCalculadora/MiniCalculadora.java
@@ -0,0 +1,32 @@
+package MiniCalculadora;
+
+public class MiniCalculadora {
+
+    private double insertarNumero;
+
+
+    //=================Getter && Setter======================
+
+
+    public double getInsertarNumero() {
+        return insertarNumero;
+    }
+
+    public void setInsertarNumero(double insertarNumero) {
+        this.insertarNumero = insertarNumero;
+    }
+
+    //===============Constructor=======================
+
+    public MiniCalculadora(double insertarNumero) {
+        this.insertarNumero = insertarNumero;
+    }
+
+
+    //===========MetodosCruds=============================
+
+    public double raizCuadrada (){
+        return  Math.sqrt(insertarNumero);
+    }
+
+}
